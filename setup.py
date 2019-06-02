23aff88c03adf89cbd41499191ce39d475586c78
Alex Cross <alexjcross90@gmail.com>
2019-06-02 23:10:02 +0100

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Package meta-data.
+NAME = 'moonshine'
+DESCRIPTION = 'My short description for my project.'
+URL = r'https://github.com/CatchemAl/moonshine'
+EMAIL = 'catchemal@foobar.com'
+AUTHOR = 'Catchem AL'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1'
+
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    packages=[NAME],  # same as name
+    install_requires=REQUIRED,  # external packages as dependencies
+)
